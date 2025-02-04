aa9e5dcc6cb252fb0a431b2d1aae90d67e1b5225
Amir Aiman <amiraiman.jj@gmail.com>
2025-02-04 19:25:22 +0800

    add maze main function to draw a maze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from window import Window
-from graphics import Point, Line, Cell
+from graphics import Point, Line
+from maze import Cell, Maze
 
 
 def print_start_arrow(win, color):
@@ -91,8 +92,21 @@ def main():
 
     c3.draw_move(c4, True)
 
-    win.run()
+    win.wait_for_close()
+
+
+def maze():
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    win.wait_for_close()
 
 
 if __name__ == "__main__":
-    main()
+    maze()
